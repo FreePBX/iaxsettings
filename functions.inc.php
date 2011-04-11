002b94e43acc1250066602d0f7a9a16360fa170b
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2011-04-11 21:25:10 +0000

    fixes #5058 really this time, 4569 for iax port

--- functions.inc.php
+++ functions.inc.php
@@ -316,7 +316,7 @@ function iaxsettings_edit($iax_settings) {
       break;
 
       case 'bindport':
-        $msg = _("Bind Port (bindport) must be between 1024..65535, default 5060");
+        $msg = _("Bind Port (bindport) must be between 1024..65535, default 4569");
         $save_settings[] = array($key,$db->escapeSimple($vd->is_ip_port($val, $key, $msg)),'1',IAX_NORMAL);
       break;
 
