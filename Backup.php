fc7b8ea90228148ca2086d0a6d8497891ad4ada4
James Finstrom <jfinstrom@sangoma.com>
2018-06-26 18:14:40 +0000

    Namespace issue

--- Backup.php
+++ Backup.php
@@ -1,5 +1,5 @@
 <?php
-namespace FreePBX\modules\Iaxseettings;
+namespace FreePBX\modules\Iaxsettings;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
   public function runBackup($id,$transaction){
