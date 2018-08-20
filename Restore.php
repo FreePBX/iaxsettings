afadcfa98d02999e63f7cbe291d265b98c8e3ef9
James Finstrom <jfinstrom@sangoma.com>
2018-08-20 17:54:47 +0000

    Fix Syntax error

--- Restore.php
+++ Restore.php
@@ -1,11 +1,11 @@
+<?php
+namespace FreePBX\modules\Iaxsettings;
 // vim: set ai ts=4 sw=4 ft=php:
 /*
  * License for all code of this FreePBX module can be found in the license file inside the module directory
  * Copyright (c) 2018 Sangoma Technologies
  */
 
-<?php
-namespace FreePBX\modules\Iaxsettings;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
